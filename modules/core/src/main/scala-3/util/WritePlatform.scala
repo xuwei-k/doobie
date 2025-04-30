3c73690c8a1d6c101aae4e6f54a64894572d1fb6
xuwei-k <6b656e6a69@gmail.com>
2025-04-30 20:19:52 +0900

    Write

--- modules/core/src/main/scala-3/util/WritePlatform.scala
+++ modules/core/src/main/scala-3/util/WritePlatform.scala
@@ -5,29 +5,43 @@
 package doobie.util
 
 import scala.deriving.Mirror
+import scala.compiletime.summonAll
 
-trait WritePlatform:
+object WritePlatform {
 
-  // Trivial write for empty tuple.
-  given Write[EmptyTuple] =
-    new Write(Nil, _ => Nil, (_, _, _) => (),(_, _, _) => ())
+  def productImpl[A <: Product](mirror: Mirror.ProductOf[A], values: Tuple.Map[mirror.MirroredElemTypes, Write]): Write[A] = {
+    val writes: List[Write[Any]] = values.toList.map(_.asInstanceOf[Write[Any]])
+    new Write[A](
+      writes.flatMap(_.puts),
+      { (x: A) =>
+        x.productIterator.zip(writes).flatMap{
+          (a, w) =>
+            w.toList(a)
+        }.toList
+      },
+      (ps, n, x) =>
+        writes.zip(x.productIterator).zipWithIndex.foreach {
+          case (((w, a), i)) =>
+            w.unsafeSet(ps, n + writes.take(i).map(_.length).sum, a)
+        }
+      ,
+      (ps, n, x) =>
+        writes.zip(x.productIterator).zipWithIndex.foreach {
+          case (((w, a), i)) =>
+            w.unsafeUpdate(ps, n + writes.take(i).map(_.length).sum, a)
+        }
+    )
+  }
+}
+
+trait WritePlatform {
 
   // Inductive write for writable head and tail.
-  given [H, T <: Tuple](using H: => Write[H], T: => Write[T]): Write[H *: T] =
-    new Write(
-      H.puts ++ T.puts,
-      { case h *: t => H.toList(h) ++ T.toList(t) },
-      { case (ps, n, h *: t) => H.unsafeSet(ps, n, h); T.unsafeSet(ps, n + H.length, t) },
-      { case (rs, n, h *: t) => H.unsafeUpdate(rs, n, h); T.unsafeUpdate(rs, n + H.length, t) }
+  inline given product[A <: Product](using mirror: Mirror.ProductOf[A]): Write[A] =
+    WritePlatform.productImpl[A](
+      mirror,
+      summonAll[Tuple.Map[mirror.MirroredElemTypes, Write]]
     )
-
-  // Generic write for products.
-  given [P <: Product, A](
-    using m: Mirror.ProductOf[P],
-          i: m.MirroredElemTypes =:= A,
-          w: Write[A]
-  ): Write[P] =
-    w.contramap(p => i(Tuple.fromProductTyped(p)))
 
   // Trivial write for option of empty tuple.
   given woe: Write[Option[EmptyTuple]] =
@@ -77,3 +91,5 @@ trait WritePlatform:
           w: Write[Option[A]]
   ): Write[Option[P]] =
     w.contramap(op => op.map(p => i(Tuple.fromProductTyped(p))))
+
+}
